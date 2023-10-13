100fa73d1b54c5e332808a69576bc5b2039558ed
Nazar <83466233+tipanazar@users.noreply.github.com>
2023-10-13 17:28:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ def index():
     return render_template('index.html')
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), nullable=False)
